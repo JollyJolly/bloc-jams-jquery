355f023723249126f455090114ea6b008efca3f2
JollyJolly <jolly@elizalab.com>
2019-03-28 21:52:55 -0700

    Assignment 10 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -15,4 +15,14 @@
     player.playPause(nextSong);
   });
 
+  $('button#previous').on('click', function(){
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (currentSongIndex == 0) { return; }
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+  });
 }
